99c0e47b441a6db8ef7b189ca11ab695a637ba84
MLoth <MLoth@users.noreply.github.com>
2023-03-07 10:15:14 +0100

    feat: setup for settings

--- screens/MainStack/Settings.tsx
+++ screens/MainStack/Settings.tsx
@@ -1,11 +1,48 @@
-import { Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { Switch, SwitchChangeEvent, Text, View } from 'react-native'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import Container from '../../components/Container'
 
 export default () => {
+  const [isDark, setIsDark] = useState<boolean>(true)
+
+  const getInitialMode = async (): Promise<void> => {
+    const mode = await AsyncStorage.getItem('mode')
+    if (mode) {
+      setIsDark(true)
+    } else {
+      setIsDark(false)
+    }
+  }
+
+  useEffect(() => {
+    getInitialMode()
+  }, [])
+
+  useEffect(() => {
+    AsyncStorage.setItem('mode', isDark.toString())
+  }, [isDark])
+
   return (
     <Container>
       <Text>Settings</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Text>Mode: {isDark ? 'dark' : 'light'}</Text>
+        <Switch
+          value={isDark}
+          onChange={(event: SwitchChangeEvent) =>
+            setIsDark(event.nativeEvent.value)
+          }
+        />
+      </View>
     </Container>
   )
 }
--- screens/MainStack/index.tsx
+++ screens/MainStack/index.tsx
@@ -7,7 +7,7 @@ const Stack = createStackNavigator()
 
 export default () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Settings">
       <Stack.Screen name="Conversion" component={Conversion} />
       <Stack.Screen name="Settings" component={Settings} />
     </Stack.Navigator>
